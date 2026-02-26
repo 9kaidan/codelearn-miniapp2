a1e8d211d5511e20a6cd0c13a04de67f2c23aac8
9kaidan <alihanh89@gmail.com>
2026-02-27 00:30:42 +0300

    Create lessons.js

---
+++ js/lessons.js
@@ -0,0 +1,23 @@
+export const lessons = {
+  python: [
+    {
+      id: 1,
+      type: "theory",
+      title: "Что такое Python",
+      content: "Python — это язык программирования..."
+    },
+    {
+      id: 2,
+      type: "quiz",
+      question: "Как объявить переменную?",
+      options: ["int x = 5", "x = 5", "var x = 5"],
+      correct: 1
+    },
+    {
+      id: 3,
+      type: "theory",
+      title: "Типы данных",
+      content: "В Python есть int, str, float..."
+    }
+  ]
+};
